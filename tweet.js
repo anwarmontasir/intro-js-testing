2c91f3c766b8eecafc39d7b309c8b7cb410b1603
anwarmontasir <anwarmontasir@gmail.com>
2023-01-21 14:11:24 -0800

    17 passing tests

--- tweet.js
+++ tweet.js
@@ -1,5 +1,24 @@
 class Tweet {
+    constructor(sender, content, link = 'no link provided', likes = 0, retweets = 0, replies = []) {
+        this.sender = sender;
+        this.content = content;
+        this.link = link;
+        this.likes = likes;
+        this.retweets = retweets;
+        this.replies = replies;
+        this.replyCount = replies.length;
+    }
 
+    like(likes) {
+        this.likes++;
+    }
+    retweet(retweets) {
+        this.retweets++;
+    }
+    reply(replyContent) {
+        this.replies.push(replyContent);
+        this.replyCount++;
+    }
 }
 
 module.exports = Tweet;
